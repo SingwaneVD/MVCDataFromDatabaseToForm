31062947ad5365e901e4e40c4d3cae8b41546394
VictorSi <VictorSi@MCEN00014>
2024-08-19 11:57:04 +0200

    Add project files.

---
+++ MVCApp03/Models/EmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿4using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp03.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Email { get; set; }
+        public string ConfirmEmail{ get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        
+    }
+}
---
+++ MVCApp03/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MVCApp03")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVCApp03")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88a7ad2c-b7ae-4e60-be17-b4f55556e324")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
